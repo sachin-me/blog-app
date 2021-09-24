37c4fda66eeefcf501809725fed3cb9b6bbbb88a
sachin-me <sachinkumartiwari363@gmail.com>
2021-09-25 00:15:05 +0530

    init repo

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cors = require("cors");
+const path = require("path");
+
+const port = 3000;
+
+// connecting with mongodb
+mongoose.connect(
+  "mongodb://localhost/blog-app",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  function (err, connection) {
+    if (err) throw err;
+    else console.log("connected to mongodb");
+  }
+);
+
+// it'll parse incoming requests with JSON payloads
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// It'll log api calls in console
+app.use(morgan("dev"));
+
+app.use(cors());
+
+// running the server
+app.listen(port, () => {
+  console.log(`server is running on http://localhost:${port}`);
+});
