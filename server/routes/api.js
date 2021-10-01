63d6ff0dd1187ae3f115f73d29e675f4d709655b
sachin-me <sachinkumartiwari363@gmail.com>
2021-10-01 20:53:31 +0530

    Added user login api

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,5 +4,7 @@ const router = express.Router();
 
 // This api'll create a new user
 router.post("/signup", userController.create);
+// This api'll login the user
+router.post("/login", userController.login);
 
 module.exports = router;
