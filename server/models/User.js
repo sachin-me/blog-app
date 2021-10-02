45eabc936aa2b5352d5c08a00ddf5131fc9f862b
sachin-me <sachinkumartiwari363@gmail.com>
2021-10-02 12:27:49 +0530

    hashed password before saving to db

--- server/models/User.js
+++ server/models/User.js
@@ -1,10 +1,21 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const SALT_ROUNDS = 10;
 
 const Schema = mongoose.Schema;
 const userSchema = new Schema({
   name: { type: String },
   email: { type: String },
   password: { type: String },
+});
+
+UserSchema.pre("save", function (next) {
+  if (this.password) {
+    this.password = bcrypt.hashSync(this.password, SALT_ROUNDS);
+    next();
+  } else {
+    next();
+  }
 });
 
 const User = mongoose.model("User", userSchema);
